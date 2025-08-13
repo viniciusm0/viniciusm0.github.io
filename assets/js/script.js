0bdab0a2b2511984ed43e9d7da61d81312778518
Vinicius Moreira <viniciusm1603@gmail.com>
2025-08-13 19:28:40 -0300

    feat: add toggleTheme header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-const img = document.getElementById('img-logo')
+const imgHeader = document.getElementById('img-logo');
+const toggleTheme = document.getElementById('toggleTheme');
+const rootHtml = document.documentElement;
+function changeTheme (){
+    const currentTheme = rootHtml.getAttribute("data-theme");
+
+    if (currentTheme == "dark") {
+         rootHtml.setAttribute("data-theme", "light"), 
+        imgHeader.setAttribute("src", "assets/img/logo-light.png");
+    } else {
+        rootHtml.setAttribute("data-theme", "dark"), 
+        imgHeader.setAttribute("src", "assets/img/logo-dark.png")
+    } 
+        
+    toggleTheme.classList.toggle("bi-brightness-high-fill");
+    toggleTheme.classList.toggle("bi-moon-stars-fill");
+}
+
+toggleTheme.addEventListener("click", changeTheme);
